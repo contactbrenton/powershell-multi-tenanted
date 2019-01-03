f5b6fb6dce32e818a17c1107d46d7c9b495796ee
contactbrenton <41414737+contactbrenton@users.noreply.github.com>
2019-01-03 12:18:56 +1100

    Added modern Auth Script
    Required to make Office 365 work properly if tenant is 12 months or more old

---
+++ ModernAuth all tenants.ps1
@@ -0,0 +1,28 @@
+﻿#
+# Manage Exchange Online Delegated Tenants.ps1
+#
+
+# Setup credentials
+$cred = Get-Credential
+Connect-MsolService -Credential $cred
+
+# Get list of tenants & loop
+Get-MsolPartnerContract -All | ForEach { 
+    $tenantprefix = [string]$_.DefaultDomainName
+
+    # Configure the connection url - note the DelegatedOrg parameter
+    $ConnectionUri = "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$tenantprefix"
+    
+    # Connect to Exchange & import session
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $ConnectionUri -Credential $cred -Authentication Basic -AllowRedirection 
+    Import-PSSession $Session -AllowClobber
+
+    Get-OrganizationConfig | Format-Table -Auto Name,OAuth*
+
+    Set-OrganizationConfig -OAuth2ClientProfileEnabled $true
+
+    Get-OrganizationConfig | Format-Table -Auto Name,OAuth*
+
+    # Remove the session - avoid hitting 3 session limit
+    Remove-PSSession $Session
+}
